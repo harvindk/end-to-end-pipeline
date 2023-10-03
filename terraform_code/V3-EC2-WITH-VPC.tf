f14febbcf68f92d180e20742109a33c0620067e3
harvindk <harvindkumark@gmail.com>
2023-10-03 17:00:32 +0530

    Revert "updated slave setup"
    This reverts commit c4691a7b9ec2571ca63c84c87e2f0be08575231d.

--- terraform_code/V3-EC2-WITH-VPC.tf
+++ terraform_code/V3-EC2-WITH-VPC.tf
@@ -3,15 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-053b0d53c279acc90"
+    ami = "ami-051f7e7f6c2f40dc1"
     instance_type = "t2.micro"
     key_name = "newkey"
     vpc_security_group_ids = [ aws_security_group.demo-sg1.id ]
     subnet_id = aws_subnet.demo-public-subnet-01.id
-    for_each = toset(["jenkins-master", "build-slave", "ansible"])
-   tags = {
-     Name = "${each.key}"
-   }
 }
 
 resource "aws_security_group" "demo-sg1" {
